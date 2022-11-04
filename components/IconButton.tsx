af48aa9410170e91250413880dcfda8e78896103
Itta Funahashi <itta@itta.dev>
2022-11-04 22:54:57 +0900

    use ComponentProps on IconButton component

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import classNames from 'classnames';
 import Button from './Button';
 
-type prop = {
-  isLink?: boolean;
-  link?: string;
-  children: React.ReactNode;
-  className?: string;
-};
+type prop = React.ComponentProps<typeof Button>;
 
 const IconButton: React.FC<prop> = ({ children, className, ...props }) => {
-  return <Button className={classNames('px-2 w-12 border-none', className)}>{children}</Button>;
+  return (
+    <Button className={classNames('px-2 w-12 border-none', className)} {...props}>
+      {children}
+    </Button>
+  );
 };
 
 export default IconButton;
