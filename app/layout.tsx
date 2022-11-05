8d46f9248070978ef7c30c8b0702893f6ad39638
Itta Funahashi <itta@itta.dev>
2022-11-05 18:52:50 +0900

    move head to layout to fix error

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,14 @@ const notoSansJP = Noto_Sans_JP({ weight: '400' });
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={classNames(inter.className, notoSansJP.className)}>
+      <head>
+        <title>Frea Search</title>
+        <meta
+          name="description"
+          content="Frea Search — a clean, privacy-respecting metasearch engine"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 import Logo from 'components/Logo';
 import Button from 'components/Button';
 import IconButton from 'components/IconButton';
@@ -10,14 +9,6 @@ import { IconSearch, IconSettings } from '@tabler/icons';
 const Home: NextPage = () => {
   return (
     <div className="min-h-screen">
-      <Head>
-        <title>Frea Search</title>
-        <meta
-          name="description"
-          content="Frea Search — a clean, privacy-respecting metasearch engine"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <main className="max-w-2xl mx-auto">
         <div className="absolute top-5 right-7 flex">
           <Button link="#">ダウンロード</Button>
