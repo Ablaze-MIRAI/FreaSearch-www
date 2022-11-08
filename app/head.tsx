88ebbd5ce4979764c61f709209ae5a663a18393a
Itta Funahashi <itta@itta.dev>
2022-11-08 19:52:09 +0900

    move home head tag to head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <title>Frea Search</title>
+      <meta
+        name="description"
+        content="Frea Search — a clean, privacy-respecting metasearch engine"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,14 +8,6 @@ const notoSansJP = Noto_Sans_JP({ weight: '400', variable: '--font-noto-sans-jp'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={classNames(inter.variable, notoSansJP.variable)}>
-      <head>
-        <title>Frea Search</title>
-        <meta
-          name="description"
-          content="Frea Search — a clean, privacy-respecting metasearch engine"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </head>
       <body>{children}</body>
     </html>
   );
