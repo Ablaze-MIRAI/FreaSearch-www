aac15dda21cfc7c71b9490ec128c3cd7700758cf
Itta Funahashi <itta@itta.dev>
2022-11-20 22:16:16 +0900

    set SelectItem cursor to pointer

--- components/Select/SelectItem.tsx
+++ components/Select/SelectItem.tsx
@@ -8,7 +8,7 @@ export default ({ value, children }: { value: string; children: ReactNode }) => 
   return (
     <Select.Item
       value={value}
-      className="outline-none text-sm pl-6 pr-3 py-1.5 data-[highlighted]:bg-gray-100"
+      className="outline-none text-sm pl-6 pr-3 py-1.5 data-[highlighted]:bg-gray-100 cursor-pointer"
     >
       <Select.ItemText>{children}</Select.ItemText>
       <Select.ItemIndicator className="absolute left-1 align-middle">
