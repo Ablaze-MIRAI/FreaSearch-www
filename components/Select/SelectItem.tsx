d91394af2b88f09e49eec14996f0a9506cd51d16
Itta Funahashi <itta@itta.dev>
2022-11-20 17:08:26 +0900

    add checkbox style

--- components/Select/SelectItem.tsx
+++ components/Select/SelectItem.tsx
@@ -6,9 +6,12 @@ import { ReactNode } from 'react';
 
 export default ({ value, children }: { value: string; children: ReactNode }) => {
   return (
-    <Select.Item value={value}>
+    <Select.Item
+      value={value}
+      className="outline-none text-sm pl-6 pr-3 py-1.5 data-[highlighted]:bg-gray-100"
+    >
       <Select.ItemText>{children}</Select.ItemText>
-      <Select.ItemIndicator className="absolute">
+      <Select.ItemIndicator className="absolute left-1 align-middle">
         <IconCheck size={16} />
       </Select.ItemIndicator>
     </Select.Item>
