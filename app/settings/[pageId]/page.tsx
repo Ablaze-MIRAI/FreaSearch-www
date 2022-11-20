caebbc7a16c44a3f1c4eeebe2f9350e76727df5f
Itta Funahashi <itta@itta.dev>
2022-11-20 19:56:57 +0900

    create SettingItem component

--- app/settings/[pageId]/page.tsx
+++ app/settings/[pageId]/page.tsx
@@ -1,6 +1,7 @@
 import { settings } from './settings.utils';
 import { notFound } from 'next/navigation';
 import { Select, SelectItem } from 'components/Select';
+import SettingItem from 'components/SettingItem';
 
 function SettingsPage({ params }: { params: { pageId: string } }) {
   const currentSettings = settings.find((page) => page.pageId === params.pageId)?.settings;
@@ -11,22 +12,12 @@ function SettingsPage({ params }: { params: { pageId: string } }) {
 
   return (
     <div>
-      {currentSettings.map((setting) => (
-        // TODO: i18n
-        <div className="flex items-center">
-          <div key={setting.settingId} className="w-48">
-            {setting.settingId}
-          </div>
-          <div className="flex-grow">
-            {setting.type == 'select' ? (
-              <Select>
-                <SelectItem value="ja">日本語</SelectItem>
-                <SelectItem value="en">英語</SelectItem>
-              </Select>
-            ) : null}
-          </div>
-        </div>
-      ))}
+      <SettingItem label="言語">
+        <Select>
+          <SelectItem value="ja">日本語</SelectItem>
+          <SelectItem value="en">英語</SelectItem>
+        </Select>
+      </SettingItem>
     </div>
   );
 }
---
+++ components/SettingItem.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type prop = {
+  children: React.ReactNode;
+  label: string;
+};
+
+const SettingItem: React.FC<prop> = ({ children, label }) => {
+  return (
+    <div className="flex items-center">
+      <div className="w-48">{label}</div>
+      <div className="flex-grow">{children}</div>
+    </div>
+  );
+};
+
+export default SettingItem;
