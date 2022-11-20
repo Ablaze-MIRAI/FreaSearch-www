8736f1f54b97b3dcbf624ed3c51758e9e3221b7b
Itta Funahashi <itta@itta.dev>
2022-11-20 15:33:28 +0900

    redirect /settings to /settings/general

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/settings',
+        destination: '/settings/general',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
