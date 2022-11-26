2b873d1562efe8b65d87e673089d3c8366829c93
Itta Funahashi <itta@itta.dev>
2022-11-26 10:40:47 +0900

    define with proper name "Select"

--- components/Select/Select.tsx
+++ components/Select/Select.tsx
@@ -1,22 +1,28 @@
 'use client';
 
-import * as Select from '@radix-ui/react-select';
+import * as RadixSelect from '@radix-ui/react-select';
 import { IconChevronDown } from '@tabler/icons';
-import { ReactNode, useState } from 'react';
+import { FC, ReactNode, useState } from 'react';
 
-export default ({ children }: { children: ReactNode }) => {
+interface props {
+  children: ReactNode;
+}
+
+const Select: FC<props> = ({ children }) => {
   const [value, setValue] = useState('en');
   return (
-    <Select.Root value={value} onValueChange={setValue}>
-      <Select.Trigger className="flex justify-between w-32 items-center rounded-md border text-left text-sm px-3 py-2 outline-none focus:ring">
-        <Select.Value />
+    <RadixSelect.Root value={value} onValueChange={setValue}>
+      <RadixSelect.Trigger className="flex justify-between w-32 items-center rounded-md border text-left text-sm px-3 py-2 outline-none focus:ring">
+        <RadixSelect.Value />
         <IconChevronDown size={16} />
-      </Select.Trigger>
-      <Select.Portal>
-        <Select.Content className="bg-white drop-shadow-md p-2 rounded-md">
-          <Select.Viewport>{children}</Select.Viewport>
-        </Select.Content>
-      </Select.Portal>
-    </Select.Root>
+      </RadixSelect.Trigger>
+      <RadixSelect.Portal>
+        <RadixSelect.Content className="bg-white drop-shadow-md p-2 rounded-md">
+          <RadixSelect.Viewport>{children}</RadixSelect.Viewport>
+        </RadixSelect.Content>
+      </RadixSelect.Portal>
+    </RadixSelect.Root>
   );
 };
+
+export default Select;
