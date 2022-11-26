a211f305f6946cb39fd3df17b67b9b3008174962
Itta Funahashi <itta@itta.dev>
2022-11-26 09:46:02 +0900

    move SettingsProvider to app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,17 @@
 import '../styles/globals.css';
 import classNames from 'classnames';
 import { Inter, Noto_Sans_JP } from '@next/font/google';
+import { SettingsProvider } from 'hooks';
 
 const inter = Inter({ variable: '--font-inter' });
 const notoSansJP = Noto_Sans_JP({ weight: '400', variable: '--font-noto-sans-jp' });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={classNames(inter.variable, notoSansJP.variable)}>
-      <body>{children}</body>
-    </html>
+    <SettingsProvider>
+      <html lang="en" className={classNames(inter.variable, notoSansJP.variable)}>
+        <body>{children}</body>
+      </html>
+    </SettingsProvider>
   );
 }
--- app/settings/layout.tsx
+++ app/settings/layout.tsx
@@ -1,7 +1,6 @@
 import 'styles/globals.css';
 import { LogoIcon } from 'components/Logo';
 import { TabList, TabItem } from 'components/TabList';
-import { SettingsProvider } from 'hooks';
 import Link from 'next/link';
 
 export default function SettingsLayout({
@@ -18,23 +17,21 @@ export default function SettingsLayout({
     { pageId: 'serachengine', name: 'Serach Engine' },
   ];
   return (
-    <SettingsProvider>
-      <div className="max-w-4xl mx-auto">
-        <div className="flex items-center space-x-10 h-16">
-          <Link href="/">
-            <LogoIcon className="shrink-0 grow-0" />
-          </Link>
-          <h1 className="font-bold text-lg">Settings</h1>
-          <TabList>
-            {settings.map((page) => (
-              <TabItem href={`/settings/${page.pageId}`} isSelected={page.pageId === params.pageId}>
-                {page.name}
-              </TabItem>
-            ))}
-          </TabList>
-        </div>
-        <div className="px-20 mt-5">{children}</div>
+    <div className="max-w-4xl mx-auto">
+      <div className="flex items-center space-x-10 h-16">
+        <Link href="/">
+          <LogoIcon className="shrink-0 grow-0" />
+        </Link>
+        <h1 className="font-bold text-lg">Settings</h1>
+        <TabList>
+          {settings.map((page) => (
+            <TabItem href={`/settings/${page.pageId}`} isSelected={page.pageId === params.pageId}>
+              {page.name}
+            </TabItem>
+          ))}
+        </TabList>
       </div>
-    </SettingsProvider>
+      <div className="px-20 mt-5">{children}</div>
+    </div>
   );
 }
