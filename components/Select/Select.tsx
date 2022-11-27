170045b3403970087a5125145a3fcac0304a3975
Itta Funahashi <itta@itta.dev>
2022-11-27 21:14:24 +0900

    fix change is not rendered

--- components/Select/Select.tsx
+++ components/Select/Select.tsx
@@ -4,9 +4,17 @@ import * as RadixSelect from '@radix-ui/react-select';
 import { IconChevronDown } from '@tabler/icons';
 import { FC, useState } from 'react';
 
-const Select: FC<RadixSelect.SelectProps> = ({ children, ...props }) => {
+const Select: FC<RadixSelect.SelectProps> = ({ children, value, onValueChange, ...props }) => {
+  const [selected, setSelected] = useState<RadixSelect.SelectItemProps['value']>(value!);
+
   return (
-    <RadixSelect.Root {...props}>
+    <RadixSelect.Root
+      value={selected}
+      onValueChange={(value) => {
+        setSelected(value);
+      }}
+      {...props}
+    >
       <RadixSelect.Trigger className="flex justify-between w-32 items-center rounded-md border text-left text-sm px-3 py-2 outline-none focus:ring">
         <RadixSelect.Value />
         <IconChevronDown size={16} />
--- hooks/useSettings.tsx
+++ hooks/useSettings.tsx
@@ -1,42 +1,31 @@
 'use client';
 
-import { FC, createContext, useContext, useState } from 'react';
+import { FC, createContext, useContext, useState, useRef } from 'react';
 
 interface ISettings {
   language: 'ja' | 'en';
 
-  setSettings: (settings: ISettings) => void;
-  saveSettings: (settings: ISettings) => void;
   setLanguage: (language: 'ja' | 'en') => void;
 }
 
 class Settings implements ISettings {
   language: 'ja' | 'en' = 'ja';
-  setSettings: (settings: ISettings) => void;
-
-  constructor(setSettings: (settings: ISettings) => void) {
-    this.setSettings = setSettings;
-  }
-
-  saveSettings(settings: ISettings) {
-    this.setSettings(settings);
-  }
 
   setLanguage(language: 'ja' | 'en') {
     this.language = language;
-    this.saveSettings(this);
   }
 }
 
 const SettingsContext = createContext<Settings>(undefined!);
 
 export function useSettings() {
-  const context = useContext(SettingsContext);
-  if (context === undefined) {
+  const settings = useContext(SettingsContext);
+
+  if (settings === undefined) {
     throw new Error('useSettings must be used within SettingsProvider');
   }
 
-  return context;
+  return settings;
 }
 
 interface props {
@@ -44,7 +33,5 @@ interface props {
 }
 
 export const SettingsProvider: FC<props> = ({ children }) => {
-  const [settings, setSettings] = useState<ISettings>(undefined!);
-  if (!settings) setSettings(new Settings(setSettings));
-  return <SettingsContext.Provider value={settings}>{children}</SettingsContext.Provider>;
+  return <SettingsContext.Provider value={new Settings()}>{children}</SettingsContext.Provider>;
 };
