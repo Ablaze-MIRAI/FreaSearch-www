fcc79623ae965cd27f7b275488720ae03b6c7405
Itta Funahashi <itta@itta.dev>
2022-12-02 16:47:34 +0900

    fix keeping own state in each component

--- components/Select/Select.tsx
+++ components/Select/Select.tsx
@@ -2,20 +2,11 @@
 
 import * as RadixSelect from '@radix-ui/react-select';
 import { IconChevronDown } from '@tabler/icons';
-import { FC, useState } from 'react';
+import { FC } from 'react';
 
-const Select: FC<RadixSelect.SelectProps> = ({ children, value, onValueChange, ...props }) => {
-  const [selected, setSelected] = useState<RadixSelect.SelectItemProps['value']>(value!);
-
+const Select: FC<RadixSelect.SelectProps> = ({ children, ...props }) => {
   return (
-    <RadixSelect.Root
-      value={selected}
-      onValueChange={(value) => {
-        setSelected(value);
-        onValueChange?.(value);
-      }}
-      {...props}
-    >
+    <RadixSelect.Root {...props}>
       <RadixSelect.Trigger className="flex justify-between w-32 items-center rounded-md border text-left text-sm px-3 py-2 outline-none focus:ring">
         <RadixSelect.Value />
         <IconChevronDown size={16} />
