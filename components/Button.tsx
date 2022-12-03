9eb570ba0d29bcf7def6da00a16a174ede119eff
Itta Funahashi <itta@itta.dev>
2022-12-03 11:11:59 +0900

    set button hover color more darker

--- components/Button.tsx
+++ components/Button.tsx
@@ -26,7 +26,7 @@ const Button: React.FC<props> = ({ children, className, ...props }) => {
   return (
     <ButtonElement
       className={classNames(
-        'flex items-center justify-center border border-brand hover:bg-gray-100/70 h-11 px-5 rounded-md transition',
+        'flex items-center justify-center border border-brand hover:bg-gray-200/50 h-11 px-5 rounded-md transition',
         { 'border-none': props.variant === 'ghost' },
         className
       )}
