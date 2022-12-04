d9f824910cb850c58080f6fbfb8d84e8e3d4d3d5
Itta Funahashi <itta@itta.dev>
2022-12-04 13:27:49 +0900

    add as prop to Button component

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,21 @@ import IconButton from 'components/IconButton';
 import SearchInput from 'components/SearchInput';
 import Footer from 'components/Footer';
 import { IconSettings } from '@tabler/icons';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
   return (
     <div className="min-h-screen">
       <main className="max-w-2xl mx-auto">
         <div className="absolute top-5 right-7 flex">
-          <Button link="#">ダウンロード</Button>
-          <IconButton link="/settings" className="ml-5">
-            <IconSettings />
-          </IconButton>
+          <Link href="#" legacyBehavior>
+            <Button as="a">ダウンロード</Button>
+          </Link>
+          <Link href="/settings" legacyBehavior>
+            <IconButton as="a" className="ml-5">
+              <IconSettings />
+            </IconButton>
+          </Link>
         </div>
         <div className="mt-48 flex justify-center">
           <Logo />
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import React from 'react';
+import { FC } from 'react';
 import classNames from 'classnames';
 import Link, { LinkProps } from 'next/link';
 
@@ -8,33 +8,35 @@ export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   link?: string;
   children: React.ReactNode;
   className?: string;
-  onClick: () => void;
+  as?: keyof JSX.IntrinsicElements;
   variant?: 'outline' | 'ghost';
 }
 
-const ButtonElement: React.FC<ButtonProps> = ({ link, children, ...props }) => {
-  return link === undefined ? (
-    <button {...props}>{children}</button>
-  ) : (
-    <Link href={link} {...props}>
-      {children}
-    </Link>
-  );
-};
+const Button: FC<ButtonProps> = ({ as, children, className, ...props }) => {
+  const CustomElement = ({ ...props }) => {
+    const Tag = as as keyof JSX.IntrinsicElements;
+    return <Tag {...props} />;
+  };
 
-const Button: React.FC<ButtonProps> = ({ children, className, ...props }) => {
-  return (
-    <ButtonElement
-      className={classNames(
-        'flex items-center justify-center border border-brand hover:bg-gray-200/50 h-11 px-5 rounded-md transition',
-        { 'border-none': props.variant === 'ghost' },
-        className
-      )}
-      {...props}
-    >
-      {children}
-    </ButtonElement>
+  const buttonClassName = classNames(
+    'flex items-center justify-center border border-brand hover:bg-gray-200/50 h-11 px-5 rounded-md transition',
+    { 'border-none': props.variant === 'ghost' },
+    className
   );
+
+  if (as) {
+    return (
+      <CustomElement className={buttonClassName} {...props}>
+        {children}
+      </CustomElement>
+    );
+  } else {
+    return (
+      <button className={buttonClassName} {...props}>
+        {children}
+      </button>
+    );
+  }
 };
 
 export default Button;
