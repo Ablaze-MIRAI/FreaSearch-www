3ade1b7675bad6c82b9806bab59521d95d154e38
Itta Funahashi <itta@itta.dev>
2022-12-05 17:00:15 +0900

    show search results

--- app/search/SearchResults.tsx
+++ app/search/SearchResults.tsx
@@ -10,7 +10,22 @@ const SearchResults: FC<Props> = async ({ searchParams }) => {
   const data = await fetch(`${API_URL}/search?${searchParams.toString()}`).then((res) =>
     res.json()
   );
-  return <div>検索結果: {data.results.length}件</div>;
+  return (
+    <div>
+      <div className="mt-5 text-gray-600 text-sm">検索結果: {data.results.length}件</div>
+      <div className="flex flex-col space-y-3 mt-3">
+        {data.results.map((result: { id: string; url: string; title: string; content: string }) => (
+          <div key={result.url} className="flex flex-col py-2">
+            <a href={result.url} className="text-blue-700">
+              {result.title}
+            </a>
+            <span className=" text-xs">{result.url}</span>
+            <span className="text-gray-600 text-xs">{result.content}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SearchResults;
