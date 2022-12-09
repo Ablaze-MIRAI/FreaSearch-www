81ff37e99f667487c2312832e59fcd656ca45a4c
Itta Funahashi <itta@itta.dev>
2022-12-10 08:43:43 +0900

    show message when theres no result

--- app/search/SearchResults.tsx
+++ app/search/SearchResults.tsx
@@ -9,7 +9,7 @@ interface Props {
 const SearchResults: FC<Props> = async ({ searchParams }) => {
   const API_URL = process.env.API_URL;
   const apiParams = new URLSearchParams(searchParams);
-  apiParams.set('pageno', apiParams.get('page') || '0');
+  apiParams.set('pageno', apiParams.get('page') || '1');
   const res = await fetch(`${API_URL}/search?${apiParams.toString()}`);
   const data = await res.json();
 
@@ -17,16 +17,23 @@ const SearchResults: FC<Props> = async ({ searchParams }) => {
     return (
       <div className="py-7">
         <div className="text-gray-600 text-sm">検索結果: {data.results.length}件</div>
-        <div className="flex flex-col space-y-3 mt-5 bg-white rounded-xl px-7 py-5 w-[700px] shadow-[0_3px_25px_-2px_rgba(0,0,0,0.1),0_5px_10px_-6px_rgba(0,0,0,0.1)]">
-          {data.results.map(
-            (
-              result: { url: string; title: string; content: string; engine: string },
-              index: number
-            ) => (
-              <SearchResultItem index={index} key={index} {...result} />
-            )
-          )}
-        </div>
+        {data.results.length === 0 ? (
+          <div className="text-gray-600 mt-2">
+            <p>{apiParams.get('q')}と一致する検索結果が見つかりませんでした...。</p>
+            <p>別のキーワードでお試しください。</p>
+          </div>
+        ) : (
+          <div className="flex flex-col space-y-3 mt-5 bg-white rounded-xl px-7 py-5 w-[700px] shadow-[0_3px_25px_-2px_rgba(0,0,0,0.1),0_5px_10px_-6px_rgba(0,0,0,0.1)]">
+            {data.results.map(
+              (
+                result: { url: string; title: string; content: string; engine: string },
+                index: number
+              ) => (
+                <SearchResultItem index={index} key={index} {...result} />
+              )
+            )}
+          </div>
+        )}
       </div>
     );
   } else {
