b704f2cb49812889462ec834b1e01358bb6f93e3
Itta Funahashi <itta@itta.dev>
2022-12-16 16:25:13 +0900

    change RadixSwitch to SwitchPrimitive

--- components/Switch.tsx
+++ components/Switch.tsx
@@ -1,16 +1,16 @@
 'use client';
 
 import { FC } from 'react';
-import * as RadixSwitch from '@radix-ui/react-switch';
+import * as SwitchPrimitive from '@radix-ui/react-switch';
 import classNames from 'classnames';
 
-export interface SwitchProps extends RadixSwitch.SwitchProps {
+export interface SwitchProps extends SwitchPrimitive.SwitchProps {
   className?: string;
 }
 
 const Switch: FC<SwitchProps> = ({ className, ...props }) => {
   return (
-    <RadixSwitch.Root
+    <SwitchPrimitive.Root
       className={classNames(
         'group w-12 block h-6 p-0.5 rounded-full border-2 bg-gray-400 border-gray-300 transition',
         'data-[state=checked]:bg-green-400 data-[state=checked]:border-green-300',
@@ -18,13 +18,13 @@ const Switch: FC<SwitchProps> = ({ className, ...props }) => {
       )}
       {...props}
     >
-      <RadixSwitch.Thumb
+      <SwitchPrimitive.Thumb
         className={classNames(
           'block w-5 h-full bg-white shadow-sm rounded-full',
-          ' group-data-[state=checked]:translate-x-full transition'
+          'group-data-[state=checked]:translate-x-full transition'
         )}
       />
-    </RadixSwitch.Root>
+    </SwitchPrimitive.Root>
   );
 };
 
