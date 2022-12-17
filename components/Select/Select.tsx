e5452e1c702134ad3c7b9c79bdcc7fd470a3a6d4
Itta Funahashi <itta@itta.dev>
2022-12-17 09:42:56 +0900

    change select style in dark theme

--- components/Select/Select.tsx
+++ components/Select/Select.tsx
@@ -7,12 +7,12 @@ import { FC } from 'react';
 const Select: FC<SelectPrimitive.SelectProps> = ({ children, ...props }) => {
   return (
     <SelectPrimitive.Root {...props}>
-      <SelectPrimitive.Trigger className="flex justify-between w-32 items-center rounded-md border text-left text-sm px-3 py-2 outline-none focus:ring">
+      <SelectPrimitive.Trigger className="flex justify-between w-32 items-center rounded-md border dark:border-none dark:bg-neutral-800 text-left text-sm px-3 py-2 outline-none focus:ring">
         <SelectPrimitive.Value />
         <IconChevronDown size={16} />
       </SelectPrimitive.Trigger>
       <SelectPrimitive.Portal>
-        <SelectPrimitive.Content className="bg-white dark:bg-neutral-800 shadow-md dark:shadow-md p-2 rounded-md">
+        <SelectPrimitive.Content className="bg-white dark:bg-neutral-800 shadow-md dark:shadow-none p-2 rounded-md">
           <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
         </SelectPrimitive.Content>
       </SelectPrimitive.Portal>
