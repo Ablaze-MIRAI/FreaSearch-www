40b0183825b4155ca488bc6c1411e957aff0118c
Itta Funahashi <itta@itta.dev>
2022-12-28 08:27:44 +0900

    union component to page

--- app/settings/design/page.tsx
+++ app/settings/design/page.tsx
@@ -1,9 +1,22 @@
-import SettingsWidget from './SettingsWidget';
+'use client';
+
+import { Select, SelectItem } from 'components/Select';
+import SettingItem from 'components/SettingItem';
+import { useSettings } from 'hooks';
 
 function DesignSettingsPage() {
+  const settings = useSettings();
   return (
     <div>
-      <SettingsWidget />
+      <SettingItem label="テーマ">
+        <Select
+          value={settings.theme}
+          onValueChange={(theme: 'dark' | 'light') => settings.setTheme(theme)}
+        >
+          <SelectItem value="light">ライトテーマ</SelectItem>
+          <SelectItem value="dark">ダークテーマ</SelectItem>
+        </Select>
+      </SettingItem>
     </div>
   );
 }
--- app/settings/general/page.tsx
+++ app/settings/general/page.tsx
@@ -1,9 +1,36 @@
-import SettingsWidget from './SettingsWidget';
+'use client';
+
+import { Select, SelectItem } from 'components/Select';
+import SettingItem from 'components/SettingItem';
+import Switch from 'components/Switch';
+import { useSettings } from 'hooks';
 
 function GeneralSettingsPage() {
+  const settings = useSettings();
   return (
     <div>
-      <SettingsWidget />
+      <SettingItem label="検索言語">
+        <Select
+          value={settings.language}
+          onValueChange={(language: 'ja-JP' | 'en') => settings.setLanguage(language)}
+        >
+          <SelectItem value="ja-JP">🇯🇵 日本語</SelectItem>
+          <SelectItem value="en">🇺🇸 English</SelectItem>
+          <SelectItem value="zh">🇨🇳 中文</SelectItem>
+        </Select>
+      </SettingItem>
+      <SettingItem label="検索結果を新しいタブで開く">
+        <Switch
+          checked={settings.newTab}
+          onCheckedChange={(newTab: boolean) => settings.setNewTab(newTab)}
+        />
+      </SettingItem>
+      <SettingItem label={'"/"キーで検索'}>
+        <Switch
+          checked={settings.shortcut}
+          onCheckedChange={(shortcut: boolean) => settings.setShortcut(shortcut)}
+        />
+      </SettingItem>
     </div>
   );
 }
